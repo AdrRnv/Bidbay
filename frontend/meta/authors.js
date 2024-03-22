d7c8d6927282524867183ce9f6f82d64a69b9d9d
KiwisDev <gaelvolatier@gmail.com>
2024-03-22 09:30:19 +0100

    Update authors.js

--- frontend/meta/authors.js
+++ frontend/meta/authors.js
@@ -1,5 +1,5 @@
 // Noms et numéros d'étudiant
-export const authors = ["John Doe (12345678)", "Jane Doe (12345678)"];
+export const authors = ["Rabian-Vicaire Adrian (12207394)", "Volatier Gaël (12201350)", "Cabanne Antoine (12204912)"];
 
 // Groupe
 export const group = 0;
