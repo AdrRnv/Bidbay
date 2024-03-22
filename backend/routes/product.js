8b6f9e88ab46c123a66bd0d7ec5877ae555349c6
KiwisDev <gaelvolatier@gmail.com>
2024-03-22 12:00:23 +0100

    /api/products/:productId

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -32,13 +32,38 @@ router.get('/api/products', async (req, res, next) => {
 })
 
 router.get('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  const productId = req.params.productId
+
+  const product = await Product.findOne({
+    attributes: ['id', 'name', 'description', 'pictureUrl', 'originalPrice'],
+    include: [{
+      model: User,
+      as: 'seller',
+      attributes: ['id', 'username']
+    },
+    {
+      model: Bid,
+      as: 'bids',
+      attributes: ['id', 'price'],
+      include: [{
+        model: User,
+        as: 'bidder',
+        attributes: ['id', 'username']
+      }]
+    }],
+    where: { id: productId }
+  })
+
+  if (product === null) {
+    res.status(404).send('Product not found found')
+  }
+  res.status(200).send(product)
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
 router.post('/api/products', (req, res) => {
-  res.status(600).send()
+
 })
 
 router.put('/api/products/:productId', async (req, res) => {
